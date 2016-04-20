9b5dd589a308b7dbd26cc3547e902bc1c660d34b
ChaoYueWu <wcyontherun@163.com>
2016-04-20 23:03:48 +0800

    array partition branch coverage up to 100%

--- 软件测试/机考/模拟考试/ArrayPartition/junit/ArrayPartitionTest.java
+++ 软件测试/机考/模拟考试/ArrayPartition/junit/ArrayPartitionTest.java
@@ -18,6 +18,15 @@ public class ArrayPartitionTest {
 		ap.partition();
 		assertTrue(ap.isWeaklySorted());
 		//像xxx &&xxx这种一般应该都会有1/4覆盖不到吧？但是为什么最后这条语句通过了呢？照理来说应该返回false啊？
+		
+		int[] a3 = new int[]{2,2,2,2,};
+		ap = new ArrayPartition(a3);
+		ap.partition();
+
+		int[] a4 = new int[]{2,3,2,2,};
+		ap = new ArrayPartition(a4);
+		assertEquals(false,ap.isWeaklySorted());
+		
 	}
 
 }
